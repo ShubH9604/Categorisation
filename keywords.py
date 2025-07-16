da01b8b9fde5b461435621fcf02230a317352fcd
ShubH9604 <153529720+ShubH9604@users.noreply.github.com>
2025-07-17 03:34:55 +0530

    Create keywords.py

--- keywords.py
+++ keywords.py
@@ -1,63 +1,72 @@
 {
     "bounce_keywords": {
-      "BOUNCE CHARGES": {
-        "type_keywords": ["bounce charges", "cheque", "chq", "imps", "rtgs", "nach", "ach", "ecs"],
-        "keywords": [
-          "achdebitreturncharges", "ACH RTN CHRG", "rtn chgs", "return chgs", "ret chgs", "rtn chrgs",
-          "return chrgs", "ret chrgs", "rtn charges", "return charges", "retcharges", "ret charges",
-          "bounce charges", "debit return", "credit return", "rtnchg", "bounced charges", "rtn chrg", "rtn_chrg"
-        ]
-      },
       "BOUNCE CHARGES - GST": {
-        "type_keywords": ["bounce charges-gst", "achdebitreturnchargesgst"],
+        "type_keywords": ["bounce charges-gst", "achdebitreturnchargesgst", "gst", ".achdebitreturnchargesgst"],
         "keywords": [
-          "rtn chgs gst", "return chgs gst", "ret chgs gst", "rtn chrgs gst",
-          "return chrgs gst", "ret chrgs gst"
+          "retchg gst", "retchrg gst", "retchrgs gst", "retcharges gst", "retchgs gst",
+          "revchg gst", "revchrg gst", "revchrgs gst", "revcharges gst", "revchgs gst",
+          "rtnchg gst", "rtnchrg gst", "rtnchrgs gst", "rtncharges gst", "rtnchgs gst",
+          "returnchg gst", "returnchrg gst", "returnchrgs gst", "returncharges gst", "returnchgs gst"
         ]
       },
-      "CHEQUE": {
+      "BOUNCE CHARGES": {
+        "type_keywords": ["bounce charges", "cheque", "chq", "imps", "rtgs", "nach", "ach", "ecs", "chg", "chrgs", "chrg", "chgs", "charges", ".achdebitreturncharges"],
+        "keywords": [
+          "achdebitreturncharges", "ACH RTN CHRG", ".achdebitreturncharges",
+          "retchg", "retchrg", "retchrgs", "retcharges", "retchgs",
+          "revchg", "revchrg", "revchrgs", "revcharges", "revchgs",
+          "rtnchg", "rtnchrg", "rtnchrgs", "rtncharges", "rtnchgs",
+          "returnchg", "returnchrg", "returnchrgs", "returncharges", "returnchgs",
+          "ret charges", "ret_chrg", "rtn_chrg", "rev_chrg", "rtn chrg",
+          "bounce charges", "debit return", "credit return", "bounced charges", "chq return", "Chrg: Chq Issue And Return", "return charges"
+        ]
+      },
+      "CHEQUE BOUNCE - TECHNICAL": {
         "type_keywords": ["chq", "cheque", "returned", "inward return", "reject"],
-        "technical_keywords": [
-          "instrument undated", "signature differs", "payment stopped",
-          "payment stopped by drawer", "exceeds arrangement"
-        ],
-        "non_technical_keywords": [
-          "chq ret", "chq return", "funds insufficient", "i/w chq ret",
-          "i/w chq rtn", "i/w chq return", "o/w rtn chq", "rtn(chq no.)", "chq issued bounce",
-          "reject:(chq no.)", "ow chq rej", "brn-ow rtn", "brn-ow rtn clg", "i/wchqreturn",
-          "reject:funds insufficient", "returned:funds insufficient", "funds", "chqretfunds"
+        "keywords": [
+          "instrument undated", "signature differs", "signature mismatch", "drawer’s signature differs",
+          "payment stopped", "payment stopped by drawer", "stop payment", "exceeds arrangement", "exceeds limit", "signature", "instrument", "instrument outdated", "rej"
+        ]
+      },
+      "CHEQUE BOUNCE - NON TECHNICAL": {
+        "type_keywords": ["chq", "cheque", "returned", "inward return", "reject"],
+        "keywords": [
+          "chq ret", "chq return", "funds insufficient", "insufficient funds", "fundsinsufficient",
+          "i/w chq ret", "i/w chq rtn", "i/w chq return", "i/wchqreturn",
+          "o/w rtn chq", "ow chq rej", "brn-ow rtn", "brn-ow rtn clg",
+          "rtn(chq no.)", "reject:(chq no.)", "reject:funds insufficient",
+          "returned:funds insufficient", "chq issued bounce", "funds", "chqretfunds",
+          "inward return", "rejected", "other reason"
         ]
       },
       "ACH BOUNCE": {
         "type_keywords": ["ach", "nach"],
-        "keywords": ["nach return", "nach rtn", "rev:nach", "achdebit rtn"]
+        "keywords": ["nach return", "nach rtn", "rev:nach", "achdebit rtn", "return", "reject", "rev", "reversal", "returned"]
       },
       "NEFT BOUNCE": {
         "type_keywords": ["neft"],
         "keywords": [
-          "account does not exist", "rej", "rtn", "ret", "return", "return(account closed)",
+          "account does not exist", "return(account closed)",
           "return(not a valid cpin)", "returned", "returnfor", "reversal",
-          "rtn(blocked account)", "rtn(invalid account)"
+          "rtn(blocked account)", "rtn(invalid account)", "rev", "return", "rtn"
         ]
       },
       "IMPS BOUNCE": {
         "type_keywords": ["imps"],
-        "keywords": ["imps return", "rev:imps", "reversal", "ret", "rtn", "failed", "rev-imps", "return-imps", "return", "rev", "rej"]
+        "keywords": ["imps return", "rev:imps", "reversal", "ret", "rtn", "failed", "rev-imps", "return-imps", "return", "rev", "rej", "returned", "paymentreturn", "mbloanreturn", "mbremainingloanreturn"]
       },
       "UPI BOUNCE": {
         "type_keywords": ["upi"],
-        "keywords": ["return", "ret", "rtn", "returned", "failed", "rev", "reversal", "fail", "failur", "faild"]
+        "keywords": ["return", "ret", "rtn", "returned", "failed", "rev", "reversal", "fail", "failur", "faild", "reversal", "returned", "revupi", "loanreturn"]
       },
       "RTGS BOUNCE": {
         "type_keywords": ["rtgs"],
-        "keywords": ["rtgs return", "rtgs failed", "rtn:rtgs", "rev"]
+        "keywords": ["rtgs return", "rtgs failed", "rtn:rtgs", "rev", "return", "reject", "reversal", "returned"]
       },
       "ECS BOUNCE": {
         "type_keywords": ["ecs"],
-        "keywords": ["ecs return", "return ach", "nach fail", "inward return"]
+        "keywords": ["ecs return", "return ach", "nach fail", "inward return", "return", "reject", "rev", "reversal", "returned"]
       }
     },
-    "loan_keywords": [
-      "emi", "achd", "repay", "instalment", "installment", "finance", "nbfc"
-    ]
+    "loan_keywords": ["emi", "achd", "repay", "instalment", "installment", "finance", "nbfc"]
   }
